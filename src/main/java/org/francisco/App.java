2b9c0df3f852041780aeb242dd4d8e9367b95bc6
Francisco J. Becerra <82810288+franbecpin@users.noreply.github.com>
2025-06-20 15:00:58 +0200

    Add files via upload

---
+++ src/main/java/org/francisco/App.java
@@ -0,0 +1,10 @@
+package org.francisco;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/module-info.java
@@ -0,0 +1,5 @@
+module tutorial_maven_jar {
+	exports org.francisco;
+
+	requires org.junit.jupiter.api;
+}
