9716ae7cd4f55cc949d8e1ec370c4f4660e94bfa
Francisco J. Becerra <82810288+franbecpin@users.noreply.github.com>
2025-06-20 12:58:10 +0200

    Update App.java

--- src/main/java/com/mycompany/App.java
+++ src/main/java/com/mycompany/App.java
@@ -8,6 +8,6 @@ public class App
     public static void main( String[] args )  
     {  
         System.out.println( "Hello World!" );  
-        Systen.out.println("kdkd);
+        Systen.out.println("kdkd");
     }  
 }  
